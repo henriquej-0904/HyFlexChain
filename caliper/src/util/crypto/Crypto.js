cdd059dc304fb76da3739d14e9d45a6ac1c2645c
henriquej-0904 <hjp.ferreira@campus.fct.unl.pt>
2023-04-02 17:05:26 +0000

    Add crypto utils

---
+++ caliper/src/util/crypto/Crypto.js
@@ -0,0 +1,121 @@
+'use strict';
+
+const crypto = require("node:crypto");
+const KeyPair = require("./KeyPair");
+
+
+class Crypto
+{
+	PASSPHRASE = "top secret";
+
+	/**
+	 * Generate a EC key pair.
+	 * @returns {KeyPair} key pair
+	 */
+	genKeyPairEC() {
+		let res = crypto.generateKeyPairSync(
+			"ec",
+			{
+				namedCurve: "secp256r1"
+			}
+		);
+
+		return new KeyPair(res.publicKey, res.privateKey);
+	}
+
+	/**
+	 * Encode a public key
+	 * @param {crypto.KeyObject} pubKey 
+	 * @returns {string} an encoded format of the key
+	 */
+	encodePublicKey(pubKey)
+	{
+		return pubKey.export(
+			{
+				type: "spki",
+				format: "pem"
+			}
+		);
+	}
+
+	/**
+	 * Encode a private key
+	 * @param {crypto.KeyObject} privKey 
+	 * @returns {string} an encoded format of the key
+	 */
+	encodePrivateKey(privKey)
+	{
+		return privKey.export(
+			{
+				type: "pkcs8",
+				format: "pem",
+				cipher: 'aes-256-cbc',
+    			passphrase: this.PASSPHRASE,
+			}
+		);
+	}
+
+	/**
+	 * Encode a key pair
+	 * @param {KeyPair} keyPair 
+	 * @returns {string[]} an encoded format of the key
+	 */
+	encodeKeyPair(keyPair)
+	{
+		return [
+			this.encodePublicKey(keyPair.getPublicKey()),
+			this.encodePrivateKey(keyPair.getPrivateKey())
+		];
+	}
+
+
+	/**
+	 * Decode a public key
+	 * @param {string} pubKey 
+	 * @returns {crypto.KeyObject} the key object
+	 */
+	decodePublicKey(pubKey)
+	{
+		return crypto.createPublicKey(
+			{
+				key: pubKey,
+				type: "spki",
+				format: "pem"
+			}
+		);
+	}
+
+	/**
+	 * Decode a private key
+	 * @param {string} privKey 
+	 * @returns {crypto.KeyObject} the key object
+	 */
+	decodePrivateKey(privKey)
+	{
+		return crypto.createPrivateKey(
+			{
+				key: privKey,
+				type: "pkcs8",
+				format: "pem",
+				cipher: 'aes-256-cbc',
+    			passphrase: this.PASSPHRASE,
+			}
+		);
+	}
+
+	/**
+	 * Decode a key pair
+	 * @param {string[]} keyPair 
+	 * @returns {KeyPair} the key pair
+	 */
+	decodeKeyPair(keyPair)
+	{
+		return new KeyPair(
+			this.decodePublicKey(keyPair[0]),
+			this.decodePrivateKey(keyPair[1])
+		);
+	}
+
+}
+
+module.exports = Crypto;
---
+++ caliper/src/util/crypto/KeyPair.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const crypto = require("node:crypto");
+
+/**
+ * A key pair of public/private keys
+ */
+class KeyPair
+{
+	/**
+	 * 
+	 * @param {crypto.KeyObject} pubKey 
+	 * @param {crypto.KeyObject} privKey 
+	 */
+	constructor(pubKey, privKey)
+	{
+		this.pubKey = pubKey;
+		this.privKey = privKey;
+	}
+
+	/**
+	 * 
+	 * @returns public key
+	 */
+	getPublicKey()
+	{
+		return this.pubKey;
+	}
+
+	/**
+	 * 
+	 * @returns private key
+	 */
+	getPrivateKey()
+	{
+		return this.privKey;
+	}
+}
+
+module.exports = KeyPair;

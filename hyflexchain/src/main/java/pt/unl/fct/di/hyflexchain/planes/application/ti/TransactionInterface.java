66d67ebdac621c4db2fe28ed92f40ecc73ea649f
henriquej-0904 <hjp.ferreira@campus.fct.unl.pt>
2023-04-24 13:56:20 +0100

    add send transaction details

--- hyflexchain/src/main/java/pt/unl/fct/di/hyflexchain/planes/application/ti/TransactionInterface.java
+++ hyflexchain/src/main/java/pt/unl/fct/di/hyflexchain/planes/application/ti/TransactionInterface.java
@@ -8,7 +8,12 @@ import pt.unl.fct.di.hyflexchain.planes.data.transaction.HyFlexChainTransaction;
 public interface TransactionInterface {
 	
 	/**
-	 * Send transaction primitive.
+	 * Send transaction primitive:
+	 * submits a transaction for verification and,
+	 * if successfull, add it to the transaction pool.
+	 * Eventually, it will be inserted on a block and ordered
+	 * through a consensus mechanism. After that, the block is
+	 * appended to the Ledger.
 	 * @param tx The transaction to send.
 	 * 
 	 * @return The generated transaction id.

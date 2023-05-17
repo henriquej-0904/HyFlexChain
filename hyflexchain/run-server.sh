dbae8b449768d8188450f6ff17fa74a34fd985e6
henriquej-0904 <hjp.ferreira@campus.fct.unl.pt>
2023-05-17 11:55:54 +0100

    add run & stop scripts

--- hyflexchain/run-server.sh
+++ hyflexchain/run-server.sh
@@ -5,6 +5,7 @@
 replicaId=$1
 
 blockmess_port=`expr 10000 + $replicaId`
+server_port=`expr 10800 + $replicaId`
 
 network_name=hyflexchain
 
@@ -15,8 +16,8 @@ contact=replica-0:10000
 
 docker network create $network_name
 
-docker run --rm -it --name $replica_name -h $replica_name  \
-	--network $network_name \
+docker run --rm -d --name $replica_name -h $replica_name  \
+	--network $network_name -p $server_port:$server_port \
 	-v "$(pwd)/tls-config/$replica_name:/app/tls-config/$replica_name" \
 	-v "$(pwd)/tls-config/truststore.pkcs12:/app/tls-config/truststore.pkcs12" \
 	-v "$(pwd)/blockmess/config:/app/config" \
@@ -24,9 +25,10 @@ docker run --rm -it --name $replica_name -h $replica_name  \
 	-v "$(pwd)/blockmess/logs:/app/blockmess-logs" \
 	-v "$(pwd)/config:/app/hyflexchain-config" \
 	henriquej0904/hyflexchain \
-	java -cp hyflexchain.jar pt.unl.fct.di.hyflexchain.planes.application.SimpleApp \
-	/app/hyflexchain-config \
+	java -cp hyflexchain.jar pt.unl.fct.di.hyflexchain.api.rest.impl.server.HyFlexChainServer \
+	$replicaId $server_port /app/hyflexchain-config \
 	-POW BLOCKMESS_PORT=$blockmess_port \
 	-POW BLOCKMESS_ADDRESS=$address \
 	-POW BLOCKMESS_CONTACT=$contact
+
 

5becdd6d19422a587cd1baa15a905772b2caa9d8
henriquej-0904 <hjp.ferreira@campus.fct.unl.pt>
2023-06-23 02:36:54 +0100

    Update committee class

--- hyflexchain/src/main/java/pt/unl/fct/di/hyflexchain/planes/consensus/mechanisms/pow/PowCommittee.java
+++ hyflexchain/src/main/java/pt/unl/fct/di/hyflexchain/planes/consensus/mechanisms/pow/PowCommittee.java
@@ -1,38 +1,25 @@
 package pt.unl.fct.di.hyflexchain.planes.consensus.mechanisms.pow;
 
+import java.util.LinkedHashSet;
 import pt.unl.fct.di.hyflexchain.planes.consensus.ConsensusMechanism;
 import pt.unl.fct.di.hyflexchain.planes.consensus.committees.Committee;
-import pt.unl.fct.di.hyflexchain.planes.consensus.committees.CommitteeElectionCriteria;
 
-public class PowCommittee implements Committee {
+public class PowCommittee extends Committee {
 
 	public static final PowCommittee COMITTEE = new PowCommittee();
-
-	private final ConsensusMechanism pow;
-
-
 
 	/**
 	 * 
 	 */
 	public PowCommittee() {
-		this.pow = ConsensusMechanism.PoW;
-	}
-
-	@Override
-	public ConsensusMechanism getConsensusMechanism() {
-		return pow;
+		super();
+		this.consensusMechanism = ConsensusMechanism.PoW;
+		this.committee = LinkedHashSet.newLinkedHashSet(1);
 	}
 
 	@Override
 	public int size() {
 		return -1;
-	}
-
-	@Override
-	public CommitteeElectionCriteria getCommitteeElectionCriteria() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'getCommitteeElectionCriteria'");
 	}
 	
 }

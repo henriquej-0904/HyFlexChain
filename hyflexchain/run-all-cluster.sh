8bfd11077fe30d83955c8f4de2603b45e7e5f999
henriquej-0904 <hjp.ferreira@campus.fct.unl.pt>
2023-08-22 12:27:01 +0100

    add lauch cluster scripts

---
+++ hyflexchain/run-all-cluster.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+# run multiple servers in the same machine
+# usage: <min_id> <max_id> <address> <contact>
+
+min_id=$1
+max_id=$2
+
+
+# echo "Starting Blockmess Servers"
+
+# ./run-blockmess_server.sh $min_id
+
+# sleep 3
+
+# for (( replicaId=$min_id+1; replicaId <= $max_id; replicaId++ ));
+# do
+#     ./run-blockmess_server.sh $replicaId $3 $4
+# done
+
+# echo "Done"
+# sleep 5
+
+echo "Starting HyFlexChain servers"
+
+./run-server-cluster.sh $min_id
+
+sleep 3
+
+for (( replicaId=$min_id+1; replicaId <= $max_id; replicaId++ ));
+do
+    ./run-server-cluster.sh $replicaId $3 $4
+done
+
+echo "Done"
+
---
+++ hyflexchain/run-blockmess_server-cluster.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+# Usage: <replicaId> <address> <contact>
+
+# Create a blockmess server
+
+replicaId=$1
+
+blockmess_port=`expr 12000 + $replicaId`
+blockmess_wrapper_port=`expr 15000 + $replicaId`
+
+network_name=hyflexchain
+
+replica_name=blockmess-server-bft-smart-$replicaId
+
+interface=eth0
+# address=$replica_name
+# contact=blockmess-server-bft-smart-0:12000
+address=$2
+contact=$3
+
+docker network create $network_name
+
+docker run --rm -d --name $replica_name -h $replica_name  \
+	--network $network_name \
+	-v "$(pwd)/blockmess/config:/app/config" \
+	-v "$(pwd)/blockmess/keys:/app/keys" \
+	-v "$(pwd)/blockmess/logs:/app/blockmess-logs" \
+	henriquej0904/blockmess-wrapper-server \
+	-Xmx1g pt.unl.fct.di.blockmess.wrapper.server.tcp.BlockmessWrapperServerTCP \
+	$blockmess_wrapper_port interface=$interface address=$address port=$blockmess_port contact=$contact
+

015ece91223367b06341fe33ec6ec858b541e3cf
henriquej-0904 <hjp.ferreira@campus.fct.unl.pt>
2023-09-02 18:04:52 +0100

    Continue...

--- hyflexchain/docker-pull-cluster.sh
+++ hyflexchain/docker-pull-cluster.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
-docker pull henriquej0904/hyflexchain:hyflexchain-tc
-docker pull henriquej0904/hyflexchain:caliper
-docker pull henriquej0904/hyflexchain:blockmess-wrapper-server-tc
+HOSTS=$(oarprint host | sort)
+
+for HOST in $HOSTS;
+do
+    echo -e "\nDocker pull to $HOST"
+    oarsh $HOST docker pull henriquej0904/hyflexchain:hyflexchain-tc
+    oarsh $HOST docker pull henriquej0904/hyflexchain:caliper
+    oarsh $HOST docker pull henriquej0904/hyflexchain:blockmess-wrapper-server-tc;
+done
+
+echo Done

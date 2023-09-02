c6912d65b7f92a29e509cc9bd5d9f1314060bf26
henriquej-0904 <hjp.ferreira@campus.fct.unl.pt>
2023-09-02 18:49:06 +0100

    update scripts and max mermory for hyflexchain in cluster

--- hyflexchain/run-all-cluster.sh
+++ hyflexchain/run-all-cluster.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+# usage: ./run-all-cluster.sh <version>
+# version: 0 (only hyflexchain nodes); 1 (blockmess servers for bft smart and hyflexchain nodes)
+
 # run multiple servers in a cluster
 
 NUM_NODES=20
@@ -8,26 +11,28 @@ HOSTS=$(oarprint host | sort | head -n 4)
 NUM_HOSTS=$(oarprint host | sort | head -n 4 | wc -l)
 NODES_PER_HOST=$(($NUM_NODES / $NUM_HOSTS))
 
-echo "Starting Blockmess Servers"
+if [ $1 -eq 1 ]; then
+    echo "Starting Blockmess Servers"
 
-NODE_IDX=0
+    NODE_IDX=0
 
-for ((i=0;i<$NODES_PER_HOST;i++));
-do
-    for HOST in $HOSTS;
+    for ((i=0;i<$NODES_PER_HOST;i++));
     do
-        oarsh $HOST "cd HyFlexChain/hyflexchain && ./run-blockmess_server-cluster.sh $NODE_IDX"
+        for HOST in $HOSTS;
+        do
+            oarsh $HOST "cd HyFlexChain/hyflexchain && ./run-blockmess_server-cluster.sh $NODE_IDX"
 
-        if [ $NODE_IDX -eq 0 ]; then
-            sleep 7;
-        fi
+            if [ $NODE_IDX -eq 0 ]; then
+                sleep 7;
+            fi
 
-        NODE_IDX=$(($NODE_IDX + 1));
-    done;
-done
+            NODE_IDX=$(($NODE_IDX + 1));
+        done;
+    done
 
-echo "Done"
-sleep 5
+    echo "Done"
+    sleep 5;
+fi
 
 #########################################################################
 
